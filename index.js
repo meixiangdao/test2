a4e0fcfe8c12e7de580b07d3b495dc4c5127e465
MeiXiangDao <yishuixs@sina.cn>
2016-03-16 21:01:07 +0800

    modify js file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.body.appendChild(document.createElement('p').appendChild(document.createTextNode('I love web')));
+var i = 0,
+    dom = document,
+    p,
+    t;
+for (; i<1000; i++) {
+    p = dom.createElement('p');
+    t = dom.createTextNode('No.' + i + '<br>');
+    p.appendChild(t);
+    dom.body.appendChild(p);
+}
