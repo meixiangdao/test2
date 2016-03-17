226544c673f2b719b3908684e17d6668cb33a618
MeiXiangDao <yishuixs@sina.cn>
2016-03-17 09:10:38 +0800

    modify js file
    i = 100000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var i = 0,
     dom = document,
     p,
     t;
-for (; i<10000; i++) {
+for (; i<100000; i++) {
     p = dom.createElement('p');
     t = dom.createTextNode('No.' + i + '<br>');
     p.appendChild(t);
